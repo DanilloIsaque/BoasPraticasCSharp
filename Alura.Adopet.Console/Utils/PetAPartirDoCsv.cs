67bd23ffe0f00278b139ed1605460635805af78d
DanilloIsaque <danilloisaq@gmail.com>
2026-01-10 23:28:13 -0300

    criando testes e entendimento

---
+++ Alura.Adopet.Console/Utils/PetAPartirDoCsv.cs
@@ -0,0 +1,19 @@
+﻿using Alura.Adopet.Console.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura.Adopet.Console.Utils
+{
+    public static class PetAPartirDoCsv
+    {
+        public static Pet ConverteDoTexto(this string linha)
+        {
+            string[]? propriedades = linha.Split(';');
+            return new Pet(Guid.Parse(propriedades[0]),
+            propriedades[1],
+            int.Parse(propriedades[2]) == 1 ? TipoPet.Gato : TipoPet.Cachorro
+            );
+        }
+    }
+}
